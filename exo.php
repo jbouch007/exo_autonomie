ee22dec470250b7f559eb0b4684882dfcf1d664e
Julian <julian.bouchetard.via@gmail.com>
2022-06-16 14:29:49 +0200

    Initial commit

---
+++ exo.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel='stylesheet' type='text/css' media='screen' href='auto.css'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+    <form method="post">
+        <div class="container">
+             <div>
+                 <div class="col" id="name">
+                     <label for="name">Votre nom :</label>
+                     <input type="text">
+                 </div>
+                 <div class="col" id="prenom">
+                     <label for="name">Votre prénom :</label>
+                     <input type="text">
+                 </div>
+                 <div class="col" id="mail">    
+                     <label for="email">Votre email :</label>
+                     <input type="email">
+                 </div>
+                 <div class="col">
+                     <label for="tel">Votre téléphone :</label>
+                     <input type="tel">
+                 </div>
+                 <div class="col">
+                     <label for="name">Votre nom :</label>
+                     <input type="text">
+                 </div>
+                 <div class="col">
+                     <textarea>Sujet</textarea>
+                 </div>
+                 <div class="col">
+                     <textarea>Message</textarea>
+                 </div>
+            </div>
+       </div>
+    </form>
+</body>
+</html>
+
+
+
+
